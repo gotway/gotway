5f85f427eab10cd15030e9dfb15a3dcb4f62cd10
Óscar Casado <oscrcc@gmail.com>
2020-05-18 18:26:08 +0100

    Add support for zap logger

--- api.go
+++ api.go
@@ -3,13 +3,13 @@ package main
 import (
 	"errors"
 	"fmt"
-	"log"
 	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"go.uber.org/zap"
 )
 
 type apiError struct {
@@ -19,24 +19,33 @@ type apiError struct {
 	Request string
 }
 
+var logger *zap.SugaredLogger
+
 func (apiError apiError) LogError() string {
 	return apiError.Error.Error() + ";" + apiError.Message + ";" + strconv.Itoa(apiError.Code) + ";" + apiError.Request
 }
 
 //NewAPI Starts a new HTTP server
 func NewAPI() {
+	initializeLogger()
 	router := mux.NewRouter()
 	router.HandleFunc("/api/salute", saluteHandler).Methods("GET")
 	router.HandleFunc("/api/health", healthHandler).Methods("GET")
 	port := getEnv("PORT", ":8080")
-	log.Println("Server listening on port", port)
-	log.Fatal(http.ListenAndServe(port, router))
+	logger.Info("Server listening on port ", port)
+	logger.Fatal(http.ListenAndServe(port, router))
+}
+
+func initializeLogger() {
+	zapLogger, _ := zap.NewDevelopment()
+	defer zapLogger.Sync()
+	logger = zapLogger.Sugar()
 }
 
 func saluteHandler(w http.ResponseWriter, r *http.Request) {
 	if rand.Intn(2)%2 == 0 {
 		apiError := apiError{errors.New("not found"), "No salute found for this request", 404, r.URL.String()}
-		log.Println(apiError.LogError())
+		logger.Error(apiError.LogError())
 		http.Error(w, "No salute for you", http.StatusNotFound)
 	} else {
 		fmt.Fprintf(w, "Hello\n")
