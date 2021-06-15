2773e0d2153920f1821a411523819c5eff41a2f3
Martín Montes <martin11lrx@gmail.com>
2021-06-15 22:31:16 +0200

    feat: updated release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,25 +4,25 @@ set -e
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-tag=$(git describe --abbrev=0 --tags)
+tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 
 function release() {
-    name="$1"
-    tag="$2"
-    path="$3"
-    image="gotwaygateway/$name"
-    platform="linux/amd64,linux/arm64,linux/arm"
+  name="$1"
+  tag="$2"
+  path="$3"
+  image="gotwaygateway/$name"
+  platform="linux/amd64,linux/arm64,linux/arm"
 
-    echo "🏗    Building '$image'. Context: '$path'"
-    docker buildx create --name "$name" --use --append
-    docker buildx build --platform "$platform" --build-arg SERVICE="$name" -t "$image:$tag" -t "$image:latest" --push "$path"
-    docker buildx imagetools inspect "$image:latest"
+  echo "🏗    Building '$image'. Context: '$path'"
+  docker buildx create --name "$name" --use --append
+  docker buildx build --platform "$platform" --build-arg SERVICE="$name" -t "$image:$tag" -t "$image:latest" --push "$path"
+  docker buildx imagetools inspect "$image:latest"
 }
 
 release "gotway" "$tag" .
 
 for ms in $(ls -d cmd/*); do
-    name=$(basename "$ms")
-    path="$ms"
-    release "$name" "$tag" "$path"
+  name=$(basename "$ms")
+  path="$ms"
+  release "$name" "$tag" "$path"
 done
