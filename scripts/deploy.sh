0823d6fcdcb58446147d7bb21e1e6e8d5346f05c
Martín Montes <martin11lrx@gmail.com>
2021-06-15 21:59:29 +0200

    feat: setting example services tag in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,4 +8,8 @@ helm repo add "$repo" https://charts.gotway.duckdns.org
 helm repo update
 
 echo "🚀 Deploying '${release}'..."
-helm upgrade --install "$release" "$repo/$release" --set image.tag=$tag
+helm upgrade --install "$release" "$repo/$release" \\
+  --set image.tag=$tag \\
+  --set catalog.image.tag=$tag \\
+  --set stock.image.tag=$tag \\
+  --set route.image.tag=$tag
