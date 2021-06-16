c895de6dbd4b96fb95269843cdb7de861f046753
Martín Montes <martin11lrx@gmail.com>
2021-06-16 23:33:23 +0200

    feat: fixed deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+repo="gotway"
 release="gotway"
-repo="gotway/gotway"
+chart="gotway/gotway"
 
 git fetch --all
 tag=$(git describe --abbrev=0 --tags)
@@ -9,8 +10,8 @@ tag=$(git describe --abbrev=0 --tags)
 helm repo add "$repo" https://charts.gotway.duckdns.org
 helm repo update
 
-echo "🚀 Deploying '$repo' with image version '$tag'..."
-helm upgrade --install "$release" "$repo" \
+echo "🚀 Deploying '$chart' with image version '$tag'..."
+helm upgrade --install "$release" "$chart" \
   --set image.tag=$tag \
   --set catalog.image.tag=$tag \
   --set stock.image.tag=$tag \
