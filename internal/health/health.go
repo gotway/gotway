9ca9d27d903f5a013c3f0c0b93f77846452c1db8
Martín Montes <martin11lrx@gmail.com>
2021-06-25 22:16:05 +0200

    Revert "feat: releasing clients on done"
    This reverts commit 229dd2586ceab5514ab2af39d8bab750adacbb73.

--- internal/health/health.go
+++ internal/health/health.go
@@ -40,7 +40,6 @@ func (h *Health) Listen(ctx context.Context) {
 		select {
 		case <-ctx.Done():
 			h.logger.Info("stopping health check")
-			h.clientFactory.Release()
 			return
 		case <-ticker.C:
 			h.logger.Debug("checking health")
