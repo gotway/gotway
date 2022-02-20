bf2df72b8b0b5642d74896153bb5e86d006bdd13
Martín Montes <martin11lrx@gmail.com>
2022-02-20 20:46:33 +0100

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,11 +7,8 @@ release="gotway"
 chart="gotway/gotway"
 namespace="gotway"
 
-git fetch --all
-tag=$(git describe --tags $(git rev-list --tags --max-count=1))
-
 helm repo add "$repo" https://charts.gotway.duckdns.org
 helm repo update
 
 echo "🚀 Deploying '$chart' with image version '$tag'..."
-helm upgrade --install "$release" "$chart" --set image.tag=$tag --namespace "$namespace"
+helm upgrade --install "$release" "$chart" --namespace "$namespace"
